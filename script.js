65a33de42a5b6ce5c7c8ed1bbee44d26878b49fa
Khoula-Khalid <143867345+Khoula-Khalid@users.noreply.github.com>
2024-01-03 16:10:31 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculateBMI() {
+  const firstNameInput = document.getElementById("firstName");
+  const heightInput = document.getElementById("height");
+  const weightInput = document.getElementById("weight");
+  const result = document.getElementById("result");
+
+  const firstName = firstNameInput.value;
+  const height = heightInput.value;
+  const weight = weightInput.value;
+  
+  const bmi = weight / height ** 2;
+  console.log(firstName);
+  console.log(weight);
+  console.log(height);
+  
+  result.innerHTML = `${firstName}, your BMI is ${bmi.toFixed(2)}!`;
+
+}
+
+
